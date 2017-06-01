8369a87423c8e4313151ff2636a3ac566e6a88a8
SlavCO <slavick.slavick@gmail.com>
2017-06-01 16:16:10 +0100

    Started on homepage

---
+++ js/app.js
@@ -0,0 +1,14 @@
+angular.module('WikendrApp', ['ngRoute', 'RouteControllers']);
+ 
+angular.module('WikendrApp').config(function($locationProvider, $routeProvider) {
+    $locationProvider.html5Mode(true);  // Enable href routing without hashes
+ 
+    $routeProvider.when('/', {
+        templateUrl: 'templates/home.html',
+        controller: 'HomeController'
+    })
+    .when('/accounts/register', {
+        templateUrl: 'templates/register.html',
+        controller: 'RegisterController'
+    });
+});
